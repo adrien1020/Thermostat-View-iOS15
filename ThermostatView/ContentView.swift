08f35d274c95e2646b69173e521194cc19a27843
Adrien <62828317+adrien1020@users.noreply.github.com>
2022-09-19 20:56:00 +0200

    Initial Commit

---
+++ ThermostatView/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ThermostatView
+//
+//  Created by Adrien Surugue on 19/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ThermostatView/ThermostatViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThermostatViewApp.swift
+//  ThermostatView
+//
+//  Created by Adrien Surugue on 19/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct ThermostatViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
